c34478e8234d38ab33c67bff4a89892526822e50
gouri rohith <137485865+rohith2414@users.noreply.github.com>
2026-07-15 22:02:53 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let map = new Map();
+        for(let i = 0; i < nums.length; i++) {
+            map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+        }
+
+        let sortedNumsBasedOnFreq = [...map.entries()].sort((a, b) => b[1] - a[1]);
+        return sortedNumsBasedOnFreq.map(n => n[0]).slice(0,k);
+    }
+}
