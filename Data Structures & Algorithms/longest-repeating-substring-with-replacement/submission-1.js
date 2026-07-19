8cf5f7dff3a715bbf836be8511a1ec88f2a03025
gouri rohith <137485865+rohith2414@users.noreply.github.com>
2026-07-20 00:05:45 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let maxLen = 0, l = 0, r = 0, maxFreq = 0;
+        let hashMap = Array.from({length: 26}, () => 0);
+
+        while(r < s.length) {
+            hashMap[s[r].charCodeAt(0) - 'A'.charCodeAt(0)]++;
+            maxFreq = Math.max(maxFreq, hashMap[s[r].charCodeAt(0) - 'A'.charCodeAt(0)]);
+
+            if((r - l + 1 - maxFreq) > k) {
+                hashMap[s[l].charCodeAt(0) - 'A'.charCodeAt(0)]--;
+                l++;
+            }
+
+            if((r - l + 1 - maxFreq) <= k) {
+                maxLen = Math.max(maxLen, r - l + 1);
+            }
+
+            r++;
+
+        }
+        return maxLen;
+    }
+}
