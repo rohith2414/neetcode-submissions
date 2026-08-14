f371095b5837192f6e15a9ab398ca3c7b7963b89
gouri rohith <137485865+rohith2414@users.noreply.github.com>
2026-08-14 14:40:57 +0530

    Add: level-order-traversal-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-0.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ *  * Definition for a binary tree node.
+  * class TreeNode {
+   *     constructor(val = 0, left = null, right = null) {
+    *         this.val = val;
+     *         this.left = left;
+      *         this.right = right;
+       *     }
+        * }
+         */
+
+         class Solution {
+             /**
+                  * @param {TreeNode} root
+                       * @return {number[][]}
+                            */
+                                levelOrder(root) {
+                                        let res = [];
+
+                                                /**
+                                                         * @param {TreeNode} node
+                                                                  * @param {number} depth
+                                                                           */
+                                                                                   function dfs(node, depth) {
+                                                                                               if (!node) return;
+
+                                                                                                           if (res.length === depth) {
+                                                                                                                           res.push([]);
+                                                                                                                                       }
+
+                                                                                                                                                   res[depth].push(node.val);
+                                                                                                                                                               dfs(node.left, depth + 1);
+                                                                                                                                                                           dfs(node.right, depth + 1);
+                                                                                                                                                                                   }
+
+                                                                                                                                                                                           dfs(root, 0);
+                                                                                                                                                                                                   return res;
+                                                                                                                                                                                                       }
+                                                                                                                                                                                                       }
+
